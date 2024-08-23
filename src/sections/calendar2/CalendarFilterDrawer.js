5eb9922c515ead8109e0e0521b66b6162da9cc43
lourd <z1309014381@gmail.com>
2024-08-23 13:51:23 +0800

    bugfix: 修复左侧过滤

--- src/sections/calendar2/CalendarFilterDrawer.js
+++ src/sections/calendar2/CalendarFilterDrawer.js
@@ -62,148 +62,148 @@ export default function CalendarFilterDrawer({
         sx: { width: 320 },
       }}
     >
-      <Stack
-        direction="row"
-        alignItems="center"
-        justifyContent="space-between"
-        sx={{ pl: 2, pr: 1, py: 2 }}
-      >
-        <Typography variant="subtitle1">过滤</Typography>
+      <div className='calendar-filter-drawer'>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          sx={{ pl: 2, pr: 1, py: 2 }}
+        >
+          <Typography variant="subtitle1">过滤</Typography>
 
-        <Tooltip title="Reset">
-          <Box sx={{ position: 'relative' }}>
-            <IconButton onClick={onResetFilter}>
-              <Iconify icon="ic:round-refresh" />
-            </IconButton>
+          <Tooltip title="Reset">
+            <Box sx={{ position: 'relative' }}>
+              <IconButton onClick={onResetFilter}>
+                <Iconify icon="ic:round-refresh" />
+              </IconButton>
 
-            {notDefault && (
-              <Box
-                sx={{
-                  top: 6,
-                  right: 4,
-                  width: 8,
-                  height: 8,
-                  borderRadius: '50%',
-                  position: 'absolute',
-                  bgcolor: 'error.main',
-                }}
+              {notDefault && (
+                <Box
+                  sx={{
+                    top: 6,
+                    right: 4,
+                    width: 8,
+                    height: 8,
+                    borderRadius: '50%',
+                    position: 'absolute',
+                    bgcolor: 'error.main',
+                  }}
+                />
+              )}
+            </Box>
+          </Tooltip>
+        </Stack>
+
+        <Divider />
+
+        <Typography
+          variant="caption"
+          sx={{
+            color: 'text.secondary',
+            fontWeight: 'fontWeightMedium',
+            p: (theme) => theme.spacing(2, 2, 1, 2),
+          }}
+        >
+          颜色
+        </Typography>
+
+        <ColorMultiPicker
+          colors={colorOptions}
+          selected={filterEventColor}
+          onChangeColor={onFilterEventColor}
+          sx={{ mx: 2 }}
+        />
+
+        <Typography
+          variant="caption"
+          sx={{
+            p: 2,
+            color: 'text.secondary',
+            fontWeight: 'fontWeightMedium',
+          }}
+        >
+          间隔
+        </Typography>
+
+        <Stack spacing={2} sx={{ px: 2 }}>
+          <DatePicker
+            label="开始时间"
+            value={picker.startDate}
+            onChange={picker.onChangeStartDate}
+            renderInput={(params) => <TextField size="small" {...params} />}
+          />
+
+          <DatePicker
+            label="结束时间"
+            value={picker.endDate}
+            onChange={picker.onChangeEndDate}
+            renderInput={(params) => (
+              <TextField
+                size="small"
+                {...params}
+                error={picker.isError}
+                helperText={picker.isError && '结束时间必须大于开始时间'}
               />
             )}
-          </Box>
-        </Tooltip>
-      </Stack>
+          />
+        </Stack>
 
-      <Divider />
+        <Typography
+          variant="caption"
+          sx={{
+            p: 2,
+            color: 'text.secondary',
+            fontWeight: 'fontWeightMedium',
+          }}
+        >
+          事件 ({events.length})
+        </Typography>
 
-      <Typography
-        variant="caption"
-        sx={{
-          color: 'text.secondary',
-          fontWeight: 'fontWeightMedium',
-          p: (theme) => theme.spacing(2, 2, 1, 2),
-        }}
-      >
-        颜色
-      </Typography>
+        <Scrollbar sx={{ height: 1 }}>
+          {orderBy(events, ['end'], ['desc']).map((event) => (
+            <ListItemButton
+              key={event._id}
+              onClick={() => onSelectEvent(event.id)}
+              sx={{ py: 1.5, borderBottom: (theme) => `dashed 1px ${theme.palette.divider}` }}
+            >
+              <Box
+                sx={{
+                  top: 16,
+                  left: 0,
+                  width: 0,
+                  height: 0,
+                  position: 'absolute',
+                  borderRight: '10px solid transparent',
+                  borderTop: `10px solid ${event.color}`,
+                }}
+              />
 
-      <ColorMultiPicker
-        colors={colorOptions}
-        selected={filterEventColor}
-        onChangeColor={onFilterEventColor}
-        sx={{ mx: 2 }}
-      />
-
-      <Typography
-        variant="caption"
-        sx={{
-          p: 2,
-          color: 'text.secondary',
-          fontWeight: 'fontWeightMedium',
-        }}
-      >
-        间隔
-      </Typography>
-
-      <Stack spacing={2} sx={{ px: 2 }}>
-        <DatePicker
-          label="开始时间"
-          value={picker.startDate}
-          onChange={picker.onChangeStartDate}
-          renderInput={(params) => <TextField size="small" {...params} />}
-        />
-
-        <DatePicker
-          label="结束时间"
-          value={picker.endDate}
-          onChange={picker.onChangeEndDate}
-          renderInput={(params) => (
-            <TextField
-              size="small"
-              {...params}
-              error={picker.isError}
-              helperText={picker.isError && '结束时间必须大于开始时间'}
-            />
-          )}
-        />
-      </Stack>
-
-      <Typography
-        variant="caption"
-        sx={{
-          p: 2,
-          color: 'text.secondary',
-          fontWeight: 'fontWeightMedium',
-        }}
-      >
-        事件 ({events.length})
-      </Typography>
-
-      <Scrollbar sx={{ height: 1 }}>
-        {orderBy(events, ['end'], ['desc']).map((event) => (
-          <ListItemButton
-            key={event._id}
-            onClick={() => onSelectEvent(event.id)}
-            sx={{ py: 1.5, borderBottom: (theme) => `dashed 1px ${theme.palette.divider}` }}
-          >
-            <Box
-              sx={{
-                top: 16,
-                left: 0,
-                width: 0,
-                height: 0,
-                position: 'absolute',
-                borderRight: '10px solid transparent',
-                borderTop: `10px solid ${event.color}`,
-              }}
-            />
-
-            <ListItemText
-              disableTypography
-              primary={
-                <Typography variant="subtitle2" sx={{ fontSize: 13, mt: 0.5 }}>
-                  {event.label}
-                </Typography>
-              }
-              secondary={
-                <Typography
-                  variant="caption"
-                  component="div"
-                  sx={{ fontSize: 11, color: 'text.disabled' }}
-                >
-                  {event.allDay ? (
-                    fDateTime(event.start)
-                  ) : (
-                    <>
-                      {`${fDateTime(event.start)} - ${fDateTime(event.end)}`}
-                    </>
-                  )}
-                </Typography>
-              }
-              sx={{ display: 'flex', flexDirection: 'column-reverse' }}
-            />
-          </ListItemButton>
-        ))}
-      </Scrollbar>
+              <ListItemText
+                disableTypography
+                primary={
+                  <Typography variant="subtitle2" sx={{ fontSize: 13, mt: 0.5 }}>
+                    {event.label}
+                  </Typography>
+                }
+                secondary={
+                  <Typography
+                    variant="caption"
+                    component="div"
+                    sx={{ fontSize: 11, color: 'text.disabled' }}
+                  >
+                    {event.allDay ? (
+                      fDateTime(event.start)
+                    ) : (
+                      <>{`${fDateTime(event.start)} - ${fDateTime(event.end)}`}</>
+                    )}
+                  </Typography>
+                }
+                sx={{ display: 'flex', flexDirection: 'column-reverse' }}
+              />
+            </ListItemButton>
+          ))}
+        </Scrollbar>
+      </div>
     </Drawer>
   );
 }
